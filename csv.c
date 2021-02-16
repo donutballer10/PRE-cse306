c70ca563da5dd820485eb19a7ae59591a0d794c0
Erik Lichtenstein <eriklich@buffalo.edu>
2021-02-16 07:44:45 -0500

    turn line into array of chars, next is to add to array of lines. haven't tested this yet

--- csv.c
+++ csv.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define LINES 3
+#define LENGTH 100
+#define CHAR 1
+
 int main(int argc, char *argv[]){
 
 	FILE * inFile = NULL;
@@ -17,11 +21,24 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 
+	// array of lines from the file
+	// pointer to array(pointer) of strings(pointer)? should i use '**' instead
+	char *lines[] = calloc(3, 100);
+
 	// print all characters in file
 	char cur = getc(inFile);
 	while (cur != EOF){
-		printf("%c", cur);
-		cur = getc(inFile);
+		//printf("%c", cur);
+		//cur = getc(inFile); // increments to next char
+		char *line = calloc(LENGTH, CHAR); 
+		char *cur_char = line; // to increment through line array
+		while (cur != "\n"){
+			*cur_char  = cur; // add char to line array
+			cur_char++; // increment to next spot in line array
+			getc(inFile); // increment to next char in file
+		}
+
+
 	}
 
 	fclose(inFile); // close file
