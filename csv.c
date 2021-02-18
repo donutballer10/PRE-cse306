fc9978fc216f6737e1d9cdedf92c2b579b568909
Amogh Joshi <amoghjos@timberlake.cse.buffalo.edu>
2021-02-18 02:44:03 -0500

    csv.c filed edited with error handeling and file openning

--- csv.c
+++ csv.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <stdlib.h>
 #include "functions/column_count.h"
 #include "functions/row_count.h"
 #include "functions/semi_group_operation.h"
 
 
-/*
-To execute the program run 
-gcc csv.c -o csv
-./csv
- */
+int main (int argc, char *argv[]){
 
-int main (void){
+  //error checking for arguments count
+  if (argc < 3){
+    printf("Invalid number of arguments\n");
+    return EXIT_FAILURE;
+  }
+
+  //pointer which contains the file name
+  char *filename = argv[argc -1]; 
+  FILE *file_ptr =fopen(filename,"r");
+
+  if (file_ptr == NULL){
+    printf("Unable to open the file\n");
+    return EXIT_FAILURE;
+  }
+  
+  //read arguments
+  for (int i =1; i<argc-1; i++){
+    char *str = argv[i];
+    printf("%s\n",str);
+  }
+
+  fclose(file_ptr);
+
+  /*
   printf("hello world! CSV!\n");
-  FILE *ptr;
   column_count(ptr);
   row_count(ptr,false);
   max(ptr,false,"hello");
   min(ptr,false,"hello");
   mean(ptr,false,"hello");
+  search(ptr,false,"hello","");                                                                                               
 
+  */
   return 0;
 }
