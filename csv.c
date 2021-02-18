64f8c62c309ec3f9541e501eb3ea520d1b0c25b4
Erik Lichtenstein <eriklich@buffalo.edu>
2021-02-18 03:25:00 -0500

    now that i can build array of lines, want to build array of line's words. pushing cause gonna change 'lines' to 'lines_arr'

--- csv.c
+++ csv.c
@@ -5,6 +5,7 @@
 #define LINES 3
 #define LENGTH 100
 #define CHAR 1
+#define WHITESPACE ' '
 
 int main(int argc, char *argv[]){
 
@@ -24,34 +25,37 @@ int main(int argc, char *argv[]){
 	// array of lines from the file
 	// pointer to array(pointer) of strings(pointer)? should i use '**' instead
 	char **lines = calloc(3, 100);
-	printf("%p = lines pointer \n", lines);
 
-	// print all characters in file
+	// build array of lines
 	char **cur_line = lines; // pointer to increment through lines array
 	char cur_char = getc(inFile); // current char in file
 	while (cur_char != EOF){
-		//printf("%c", cur);
-		char *line = calloc(LENGTH, CHAR); 
-		char *cur = line; // to increment through line array
-		//printf("%c\n", cur_char);
+
+		// build line array and add to lines array 
+		// each line array consists of the words in that line
+		char **line = calloc(LENGTH, CHAR); 
+		char **cur = line; // to increment through line array
 		while (cur_char != '\n'){
-			//printf("%c = cur_char\n", cur_char);
+
+			// build word
+			char *word = calloc(
+			while (cur_char != WHITESPACE){
+			}	
 			*cur = cur_char; // add char to line array
 			cur++; // increment to next spot in line array
 			cur_char = getc(inFile); // increment to next char in file
 		}
 		cur_char = getc(inFile); // increment to next char in file
-		//printf("%s = line\n", line);
 		*cur_line = line;
-		printf("%s = line array in loop\n", *cur_line);
 		cur_line++;
 
 	}
+
 	printf("%s = line array index 1\n", lines[0]);
 	printf("%s = line array index 2\n", lines[1]);
 	printf("%s = line array index 3\n", lines[2]);
 	
-
+	/*
 	// free each line in lines array
 	printf("%p = lines pointer later\n", lines);
   cur_line = lines; // reset it, to increment through line array, this time to free 
@@ -60,7 +64,7 @@ int main(int argc, char *argv[]){
 		//free((char *) free_cur);
 		printf("%s = line\n", (char *) cur_line);
 		cur_line++;
-	}	
+	}*/
 	free(lines); // free lines array
 	fclose(inFile); // close file
 
