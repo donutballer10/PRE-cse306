e6bf98bd25df7d574eb4ae92e15ac1d9949255b2
Amogh Joshi <amoghjos@timberlake.cse.buffalo.edu>
2021-02-18 20:47:03 -0500

    removed files

--- functions/row_count.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-void row_count(FILE *file_pointer, bool containsH){
-  printf("you're in row  count function\n");
-  return;
-}
--- functions/row_count.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef ROW_COUNT_H
-#define ROW_COUNT_H
-
-void row_count(FILE*,bool);
-
-#endif
